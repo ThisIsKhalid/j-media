01eb02ca3c96c974e2fa2a5bce5b296549e80abd
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-12-15 21:49:45 +0600

    added context api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
   return (
     <>
       <Main />
-      <Row rowId='1' title="Up Coming" fetchUrl={requests.requestUpcoming} />
-      <Row rowId='2' title="Populer" fetchUrl={requests.requestPopuler} />
-      <Row rowId='3' title="Top Rated" fetchUrl={requests.requestToprated} />
-      <Row rowId='4' title="Trending" fetchUrl={requests.requestTrending} />
+      <Row rowId="1" title="Up Coming" fetchUrl={requests.requestUpcoming} />
+      <Row rowId="2" title="Populer" fetchUrl={requests.requestPopuler} />
+      <Row rowId="3" title="Top Rated" fetchUrl={requests.requestToprated} />
+      <Row rowId="4" title="Trending" fetchUrl={requests.requestTrending} />
     </>
   );
 };
