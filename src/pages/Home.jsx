6474b6f61619ceb6c8e61642a96883ecf81c412f
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-12-15 20:39:50 +0600

    added movie section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "../Request";
 
 const Home = () => {
   return (
     <>
       <Main />
+      <Row rowId='1' title="Up Coming" fetchUrl={requests.requestUpcoming} />
+      <Row rowId='2' title="Populer" fetchUrl={requests.requestPopuler} />
+      <Row rowId='3' title="Top Rated" fetchUrl={requests.requestToprated} />
+      <Row rowId='4' title="Trending" fetchUrl={requests.requestTrending} />
     </>
   );
 };
